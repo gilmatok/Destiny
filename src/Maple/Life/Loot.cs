0351c775503b07df5853244ec6c8c1e76e504616
Gil Matok <fraysati@gmail.com>
2017-08-07 19:46:48 +0300

    Add generic Loot class to handle mob/reactor drops. Remove unused ReactorDrop class.

---
+++ src/Maple/Life/Loot.cs
@@ -0,0 +1,24 @@
+﻿using Destiny.Data;
+
+namespace Destiny.Maple.Life
+{
+    public sealed class Loot
+    {
+        public int MapleID { get; private set; }
+        public int MinimumQuantity { get; private set; }
+        public int MaximumQuantity { get; private set; }
+        public int QuestID { get; private set; }
+        public int Chance { get; private set; }
+        public bool IsMeso { get; private set; }
+
+        public Loot(Datum datum)
+        {
+            this.MapleID = (int)datum["itemid"];
+            this.MinimumQuantity = (int)datum["minimum_quantity"];
+            this.MaximumQuantity = (int)datum["maximum_quantity"];
+            this.QuestID = (int)datum["questid"];
+            this.Chance = (int)datum["chance"];
+            this.IsMeso = ((string)datum["flags"]).Contains("is_mesos");
+        }
+    }
+}
