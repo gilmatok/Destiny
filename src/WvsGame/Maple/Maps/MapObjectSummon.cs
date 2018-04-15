008317696f4505501ba7a9467ed6c4e84b5d2c0f
Bia10 <t.locke1234@gmail.com>
2018-04-16 01:43:32 +0200

    refactoring of character.cs preparations for summons

---
+++ src/WvsGame/Maple/Maps/MapObjectSummon.cs
@@ -0,0 +1,26 @@
+﻿using Destiny.Maple.Data;
+using Destiny.Maple.Life;
+using Destiny.Network;
+
+namespace Destiny.Maple.Maps
+{
+    public sealed class MapObjectSummon : MapObjects<Summon>
+    {
+        public MapObjectSummon(Map map) : base(map)
+        { }
+
+        protected override void InsertItem(int index, Summon item)
+        {
+            base.InsertItem(index, item);
+
+            if (DataProvider.IsInitialized)
+            {
+                using (Packet oPacket = item.GetCreatePacket())
+                {
+                    this.Map.Broadcast(oPacket);
+                }
+            }
+        }
+
+    }
+}
